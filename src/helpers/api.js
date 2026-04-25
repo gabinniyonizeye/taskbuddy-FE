36681baffa2f36397f8eccfbccd83fcb3b8360bc
Gabin Niyonizeye <gabinniyonizeye@gmail.com>
2026-04-25 04:00:19 +0300

    update api url to railway

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '../router'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://taskbuddy-be-production.up.railway.app/api',
   withCredentials: false
 })
 
